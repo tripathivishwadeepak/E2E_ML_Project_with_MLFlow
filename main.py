768cd57f1c68c797fb1b8d70d74cc082e3068503
vishwadeepak_tripathi <vishwa.engg@gmail.com>
2025-03-24 08:37:54 +0530

    Model_evalution with MLFLOW added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipe
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -38,6 +39,16 @@ STAGE_NAME = "Model Trainer Stage"
 try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     obj = ModelTrainerTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\n x===============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Evaluation Stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    obj = ModelEvaluationTrainingPipeline()
     obj.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\n x===============x")
 except Exception as e:
--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -3,7 +3,8 @@ from mlProject.utils.common import read_yaml,create_directories
 from mlProject.entity.config_entity import (DataIngestionConfig,
                                             DataValidationConfig,
                                             DataTransformationConfig,
-                                            ModelTrainerConfig )
+                                            ModelTrainerConfig,
+                                            ModelEvaluationConfig )
 
 
 class ConfigurationManager:
@@ -19,6 +20,7 @@ class ConfigurationManager:
 
         create_directories([self.config.artifacts_root])
 
+
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
 
@@ -32,6 +34,7 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+
 
     def get_data_validation_config(self) -> DataValidationConfig:
         config = self.config.data_validation
@@ -48,6 +51,7 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+
 
     def get_data_transformation_config(self) -> DataTransformationConfig:
         config = self.config.data_transformation
@@ -79,3 +83,22 @@ class ConfigurationManager:
             target_column = schema.name
         )
         return model_trainer_config
+
+
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+        params = self.params.ElasticNet
+        schema = self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+        
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir = config.root_dir,
+            test_data_path = config.test_data_path,
+            model_path = config.model_path,
+            all_params = params,
+            metric_file_name = config.metric_file_name,
+            target_column = schema.name,
+            mlflow_uri = "https://dagshub.com/tripathivishwadeepak/E2E_ML_Project_with_MLFlow.mlflow",
+        )
+        return model_evaluation_config
